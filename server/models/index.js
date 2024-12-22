b2bbcdef1980d36b6697f16049d9132c5433e3af
ripunjay42 <ripunjaychoudhury42@gmail.com>
2024-12-22 15:33:29 +0530

    Refactor Sequelize initialization to streamline error handling and ensure model loading only occurs if the connection is established

--- server/models/index.js
+++ server/models/index.js
@@ -10,60 +10,51 @@ const config = require(__dirname + '/../config/database.js')[env];
 const db = {};
 
 let sequelize;
+
+// Initialize Sequelize first
 try {
   if (config.use_env_variable) {
-    sequelize = new Sequelize(process.env[config.use_env_variable], {
-      ...config,
-      dialect: 'postgres'
-    });
+    sequelize = new Sequelize(process.env[config.use_env_variable], config);
   } else {
-    sequelize = new Sequelize(config.database, config.username, config.password, {
-      ...config,
-      dialect: 'postgres'
-    });
+    sequelize = new Sequelize(
+      config.database,
+      config.username,
+      config.password,
+      config
+    );
   }
-
-  // Test the connection
-  sequelize.authenticate()
-    .then(() => {
-      console.log('Database connection established successfully.');
-    })
-    .catch(err => {
-      console.error('Unable to connect to the database:', err);
-    });
-
 } catch (error) {
-  console.error('Error during Sequelize initialization:', error);
+  console.error('Sequelize initialization error:', error);
+  throw error;
 }
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (
-      file.indexOf('.') !== 0 &&
-      file !== basename &&
-      file.slice(-3) === '.js' &&
-      file.indexOf('.test.js') === -1
-    );
-  })
-  .forEach(file => {
-    try {
-      const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-      db[model.name] = model;
-    } catch (error) {
-      console.error(`Error loading model from file ${file}:`, error);
+// Only proceed with model loading if sequelize is initialized
+if (sequelize) {
+  fs.readdirSync(__dirname)
+    .filter(file => {
+      return (
+        file.indexOf('.') !== 0 &&
+        file !== basename &&
+        file.slice(-3) === '.js' &&
+        file.indexOf('.test.js') === -1
+      );
+    })
+    .forEach(file => {
+      try {
+        const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+        db[model.name] = model;
+      } catch (error) {
+        console.error(`Error loading model ${file}:`, error);
+        throw error;
+      }
+    });
+
+  Object.keys(db).forEach(modelName => {
+    if (db[modelName].associate) {
+      db[modelName].associate(db);
     }
   });
-
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    try {
-      db[modelName].associate(db);
-    } catch (error) {
-      console.error(`Error associating model ${modelName}:`, error);
-    }
-  }
-});
+}
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
